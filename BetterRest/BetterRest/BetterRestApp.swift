c03a3fbe3f1f9e247b9ebbaf14a71f8a83c60449
Ilya Kaznacheev <ilyakajava@gmail.com>
2023-04-23 14:19:03 +0300

    Initial commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+ //
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Илья Казначеев on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
